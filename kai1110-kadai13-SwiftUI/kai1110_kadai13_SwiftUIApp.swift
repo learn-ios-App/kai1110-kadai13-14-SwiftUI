561bd3a2553df2fb4b78d709e0d2031146ba4cc5
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-01-14 15:43:11 +0900

    Initial Commit

---
+++ kai1110-kadai13-SwiftUI/kai1110_kadai13_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  kai1110_kadai13_SwiftUIApp.swift
+//  kai1110-kadai13-SwiftUI
+//
+//  Created by 渡邊魁優 on 2023/01/14.
+//
+
+import SwiftUI
+
+@main
+struct kai1110_kadai13_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
